5393d09ffd906ad1f59b54bd4dad65615822a9f4
Marco B <m.brockmann@reanmo.com>
2022-02-17 11:21:26 +0100

    Created some testclasses for upcoming Router.php

---
+++ app/controller/Pages.php
@@ -0,0 +1,16 @@
+<?php
+    class Pages {
+        
+        public function __construct() {
+            echo 'This is the Pages Class.';
+        }
+        
+        public function index() {
+            echo '<br>Index';
+        }
+
+        public function about() {
+            echo '<br>Its about pages';
+        }
+    }
+    
---
+++ app/controller/Test.php
@@ -0,0 +1,15 @@
+<?php
+    class Test {
+        
+        public function __construct() {
+            echo 'This is a test';
+        }
+
+        public function index() {
+            echo '<br>Index';
+        }
+
+        public function about() {
+            echo '<br>Its about tests';
+        }
+    }
